beffbc02ea409eb7ab8679556df742e0293b7857
Alutulu <arthur.mata24@gmail.com>
2022-11-22 16:24:22 +0100

    init home page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Auto-Boat</title>
+</head>
+
+<body>
+  <h1>Notre super projet !!!</h1>
+  <p>Youhou !</p>
+</body>
+
+</html>
