8182a454ea8a154a3e4d5026fec8264c98717427
Alutulu <arthur.mata24@gmail.com>
2022-11-28 08:23:41 +0100

    delete sass

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="style.css">
   <title>Auto-Boat</title>
 </head>
 
 <body>
-  <h1>Notre super projet !!!</h1>
+  <h1>Auto-Boat</h1>
   <p>Youhou !</p>
 </body>
 
